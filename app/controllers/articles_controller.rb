0aa66913ce9fdad38014aee63ce123537ac20c00
dynnikm <mykytadynnik@gmail.com>
2022-09-18 16:32:33 +0300

    modified article_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,43 +2,48 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
-    @article = Article.find(params[:id])    
+    @article = Article.find(params[:id])
   end
+
   def new
     @article = Article.new
   end
+
   def create
-    @article = Article.new(title:"...", body:"...")
+    @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
-     def edit
-      @article = Article.find(params[:id])
-      end
+  end
 
-    def update
-      @article = Article.find(params[:id])
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-      if @article.update(article_params)
-        redirect_to @article
-      else
-        render :edit, status: :unprocessable_entity
-      end
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def destroy
+  def destroy
     @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to root_path, status: :see_other
   end
-  
-    private
+
+  private
     def article_params
       params.require(:article).permit(:title, :body)
     end
-  end
 end
