4269eee7c326ac1d387b38a8cc5a4ec39c35a320
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2022-09-25 11:03:11 +0000

    Add create comment spec

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -10,4 +10,18 @@ RSpec.feature "create article", type: :feature do
     expect(body).to have_content 'Alaska'
     expect(body).to have_content 'Comments'
   end
+
+  it 'creates a new article and comment for it' do
+    visit new_article_path
+    fill_in :article_title, with: "My first trip to Alaska"
+    fill_in :article_body, with: "dsadsa dasd asd asd as d dsadasd as dasa"
+    click_button "Create Article"
+
+    fill_in :comment_commenter, with: "John"
+    fill_in :comment_body, with: "Thanks for sharing!"
+    click_button "Create Comment"
+
+    expect(body).to have_content 'John'
+    expect(body).to have_content 'Thanks for sharing'
+  end
 end
