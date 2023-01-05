27ba1c19a0669afb5ef2b961cf58cd7d00b504f4
NikitaD <mykytadynnik@gmail.com>
2023-01-05 17:13:43 +0200

    fix regestration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
 	def index		
 	end
+	before_action :configure_permitted_parameters, if: :devise_controller?
+protected
+def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, :password_confirmation])
+
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :username, :email, :password, :remember_me])
+
+  devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :current_password])
+end	
 end
