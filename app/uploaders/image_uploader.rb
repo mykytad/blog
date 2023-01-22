81694f21a43cb8b12c6f79a66b120c3caac4e841
NikitaD <mykytadynnik@gmail.com>
2023-01-23 01:04:54 +0200

    edit registration edit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [500, 500]
   end
   version :thumb do
-    process resize_to_fit: [100, 100]
+    process resize_to_fit: [50, 50]
   end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
