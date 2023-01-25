c69976b0516194c0a9d4d2f8ca9986df5fa36da0
NikitaD <mykytadynnik@gmail.com>
2023-01-25 14:31:19 +0200

    create access branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_184216) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_230510) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
