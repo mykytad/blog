826af0d7c0fc2d80f1a5cb62534c58e6f8d0de57
NikitaD <mykytadynnik@gmail.com>
2023-01-29 11:09:43 +0200

    add user_controller comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    @avatar = Digest::SHA1.hexdigest(@user.id.to_s)
-    file_path = "public/images/#{@avatar}.png"
-    file = File.exists?(file_path)
+    @avatar = Digest::SHA1.hexdigest(@user.id.to_s) # хэш имени аватара 
+    file_path = "public/images/#{@avatar}.png" # путь к файлу аватара 
+    file = File.exists?(file_path) # проверка на существование аватара 
     if file == false
       img = Avatarly.generate_avatar(@user.name, opts={size: 40})
       File.open(file_path, 'wb') do |f| 
