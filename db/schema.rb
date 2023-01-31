c74101fecba573c4b0b1ee7ed71e681ec8247de6
NikitaD <mykytadynnik@gmail.com>
2023-01-31 14:43:25 +0200

    new comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_172004) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_112223) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_30_172004) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
