6c73ee6997a98ecf977a9b03383dd39e0b8cb522
NikitaD <mykytadynnik@gmail.com>
2023-02-04 20:03:30 +0200

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_many :articles  #foreign key - artisle_id
   has_many :comments  #foreign key - comment_id
-  include Gravtastic
-  gravtastic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
