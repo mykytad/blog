3dfdd6538fda6210a5e1e3217640026ac3bfda21
NikitaD <mykytadynnik@gmail.com>
2023-02-04 20:20:48 +0200

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  
   has_many :articles  #foreign key - artisle_id
   has_many :comments  #foreign key - comment_id
   # Include default devise modules. Others available are:
