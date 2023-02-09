4dfc8b98260c071a6459b2f06887eccb7b62da0f
NikitaD <mykytadynnik@gmail.com>
2023-02-09 13:19:33 +0200

    fix all test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :articles  #foreign key - artisle_id
-  has_many :comments  #foreign key - comment_id
+  has_many :articles  #связывает пользевателя с его статьями 
+  has_many :comments  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   describe "GET /help" do
     it "returns http success" do
-      get "/static_pages/help"
+      get "/help"
       expect(response).to have_http_status(:success)
     end
   end
